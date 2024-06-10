24bdea7fb1c4ec1fda649afcdcd47b3cf3273ede
Cece Potakey <cecepotakey24@gmail.com>
2024-06-09 23:34:55 -0400

    polishing basic styling

--- react-vite/src/components/EventsPage/EventPage.jsx
+++ react-vite/src/components/EventsPage/EventPage.jsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import { thunkCurrentEvent } from "../../redux/events";
 import { NavLink, useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { FaLocationDot } from "react-icons/fa6";
+
 
 function EventPage() {
     const dispatch = useDispatch()
@@ -29,9 +31,9 @@ function EventPage() {
                         <p> - </p>
                         <p>{currentEvent.end_time}</p>
                     </div>
-                    <h2>{currentEvent.venue}</h2>
+                    <span className="side"><FaLocationDot /> <h2> {currentEvent.venue}</h2></span>
                     <p>{currentEvent.description}</p>
-                    <p>Organized by <em>{currentEvent.organizer_name}</em></p>
+                    <p>Organized by <strong><em>{currentEvent.organizer_name}</em></strong></p>
 
                     {/* <NavLink to={`/events/${currentEvent.id}/tickets`}><button>buy ticket</button></NavLink>  */}
                     {/* ticket feature commented out for initial grading */}
