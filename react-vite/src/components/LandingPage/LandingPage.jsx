558f3c10cdc060bf9aa92b44875c4cfb1b1a3196
Cece Potakey <cecepotakey24@gmail.com>
2024-06-21 10:36:08 -0400

    styling

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -5,12 +5,14 @@ import LoginFormPage from "../LoginFormPage"
 
 function LandingPage() {
     return (
-        <section className="payment-form-background">
-            <div className="form-container">
-                <h1 className="form-title">Welcome to Afrolive</h1>
-                <LoginFormPage />
-            </div>
-        </section>
+
+        <div className="payment-form-background">
+                <div className="form-container">
+                    <h1 className="form-title">Welcome to Afrolive</h1>
+                    <LoginFormPage />
+                </div>
+        </div>
+
     )
 }
 
