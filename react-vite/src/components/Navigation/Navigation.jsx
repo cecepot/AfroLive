3fadb5f9e934ccb6f56ec9037e121ff467442a9c
Cece Potakey <cecepotakey24@gmail.com>
2024-06-21 06:55:47 -0400

    styling

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -10,21 +10,22 @@ function Navigation() {
   const location = useLocation();
   const showProfileButton = location.pathname !== "/landing" && location.pathname !== "/signup";
 
+
   return (
     showProfileButton &&
     <>
     <div className="nav-bar">
       <div>
         <NavLink to="/" className={"margin-left"}>Home</NavLink>
-        <p className={"profile-dropdown"}></p>
+        {/* <p className={"profile-dropdown"}></p> */}
       </div>
       <div>
         <p>About</p>
-        <p className={"profile-dropdown"}>coming soon !</p>
+        {/* <p className={"profile-dropdown"}>coming soon !</p> */}
       </div>
       <div>
         <p>Location</p>
-        <p className={"profile-dropdown"}>coming soon !</p>
+        {/* <p className={"profile-dropdown"}>coming soon !</p> */}
       </div>
       <div>
         <ProfileButton />
@@ -34,7 +35,7 @@ function Navigation() {
         <HiTicket className="ticket-icon"/>
         Cart
       </div>
-      <p className={"profile-dropdown"}>coming soon !</p>
+      {/* <p className={"profile-dropdown"}>coming soon !</p> */}
       </div>
     </div>
     </>
