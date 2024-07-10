f10f03879d4545976f0f157a9357ca91d2b10f82
Cece Potakey <cecepotakey24@gmail.com>
2024-07-10 09:39:05 -0400

    styling profile page

--- react-vite/src/components/UserProfilePage/ProfilePage.jsx
+++ react-vite/src/components/UserProfilePage/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux"
 import { NavLink } from "react-router-dom"
-import { IoIosCard } from "react-icons/io";
-import { IoNewspaperOutline } from "react-icons/io5";
+import { BsFillCreditCard2BackFill } from "react-icons/bs";
+import { FaRectangleList } from "react-icons/fa6";
 import { HiTicket } from "react-icons/hi2";
 
 function ProfilePage(){
@@ -10,9 +10,9 @@ const currentUser = useSelector((state) => state.session.user)
     return(
         <section className="payment-form-background profile-icons-container">
         {/* <h1 className="margin-top">User Profile Page</h1> */}
-        <NavLink className="real-container" to={`/users/${currentUser.id}/listings`}><div className="profile-page-icons">< IoNewspaperOutline /></div><span className="icon-tooltip">My Listings</span></NavLink>
+        <NavLink className="real-container" to={`/users/${currentUser.id}/listings`}><div className="profile-page-icons">< FaRectangleList/><div className="icon-tooltip">My Listings</div></div></NavLink>
         {/* <div className="real-container" ><div className="icon-coming-soon"><HiTicket/></div><span className="icon-tooltip">coming soon!</span></div> */}
-        <NavLink className="real-container" to={`/users/${currentUser.id}/cards`}><div className="profile-page-icons"><IoIosCard /></div><span className="icon-tooltip">Payment Options</span></NavLink>
+        <NavLink className="real-container" to={`/users/${currentUser.id}/cards`}><div className="profile-page-icons"><BsFillCreditCard2BackFill /><div className="icon-tooltip">Payment Options</div></div></NavLink>
         </section>
     )
 }
