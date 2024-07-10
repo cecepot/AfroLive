c5287332c3e0490a7dc6ff265137c4e7a01e8a88
Cece Potakey <cecepotakey24@gmail.com>
2024-07-10 14:08:15 -0400

    event-page styling

--- react-vite/src/components/EventsPage/EventPage.jsx
+++ react-vite/src/components/EventsPage/EventPage.jsx
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 import { thunkCurrentEvent } from "../../redux/events";
 import { NavLink, useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import { FaLocationDot } from "react-icons/fa6";
-
+import { FaFill, FaLocationDot } from "react-icons/fa6";
+import { FaClock } from "react-icons/fa6";
 
 function EventPage() {
     const dispatch = useDispatch()
@@ -17,23 +17,29 @@ function EventPage() {
 
     // const currentEvent = useSelector(state => state.event.singleEvent)
     const currentEvent = location.state.data
-    // console.log(currentEvent)
+    const start = currentEvent.start_time.slice(0, 5)
+    const end = currentEvent.end_time.slice(0, 5)
+
     return (
-        <section className="event-background">
-            <div className="event-display">
-                <div className="image-flyer">
+        <section className="event-card-container mouse">
+            <div className="event-card-sub-container">
+                <div className="event-card-front">
                     <img className='event-image' src={currentEvent.image_url} alt="" />
                 </div>
-                <div className="event-display-left">
-                    <h1>{currentEvent.title}</h1>
+                <div className="event-card-back">
+                    <h1 className="white">{currentEvent.title}</h1>
+                    <div className="side">
+                    <FaClock className="fill-white"/>
                     <div className="event-time">
-                        <p>{currentEvent.start_time}</p>
-                        <p> - </p>
-                        <p>{currentEvent.end_time}</p>
+                        <p className="white">{start}</p>
+                        <p className="white"> - </p>
+                        <p className="white">{end}</p>
                     </div>
-                    <span className="side"><FaLocationDot /> <h2> {currentEvent.venue}</h2></span>
-                    <p>{currentEvent.description}</p>
-                    <p>Organized by <strong><em>{currentEvent.organizer_name}</em></strong></p>
+                    <h2 className="white">GMT</h2>
+                    </div>
+                    <span className="side"><FaLocationDot className="fill-white" /> <h2 className="white"> {currentEvent.venue}</h2></span>
+                    <p className="white event-description">{currentEvent.description}</p>
+                    <p className="white">Organized by <strong><em className="white">{currentEvent.organizer_name}</em></strong></p>
 
                     {/* <NavLink to={`/events/${currentEvent.id}/tickets`}><button>buy ticket</button></NavLink>  */}
                     {/* ticket feature commented out for initial grading */}
