58061da8394dfb1b2e39d017dfd13b591886e69d
Cece Potakey <cecepotakey24@gmail.com>
2024-07-12 20:38:03 -0400

    card page styling

--- react-vite/src/components/PaymentOptions/PaymentOptions.jsx
+++ react-vite/src/components/PaymentOptions/PaymentOptions.jsx
@@ -70,46 +70,48 @@ function PaymentOptions() {
                 <div className="payment-container">
                     <h1 className="form-title">Payment Options</h1>
                     <p>Looks like you have no cards on your account</p>
-                    <NavLink  to={`/users/${id}/cards/add`}><button className="pay-button mouse">Add new Card</button></NavLink>
+                    <NavLink to={`/users/${id}/cards/add`}><button className="pay-button mouse">Add new Card</button></NavLink>
                 </div>
             </section>
         )
     }
 
     return (
-        <section className="payment-form-background ">
-            <div className="payment-container">
-                <h1 className="form-title">Payment Options</h1>
-                <div className="cards-container">
-                    {cards.length && cards.map((card) => {
-                        return (
-                            <div className="side" key = {card.id}>
-                                <div className="payment-icon">
-                                    <IoIosCard />
-                                </div>
-                                <div className="form centre" key={card.id}>
+        <section className="card-grand-container">
+            <h1 className="card-title">Payment Options</h1>
+            <div className="card-grid">
+                {cards.length && cards.map((card) => {
+                    return (
+                        <div className="card-main-container" key={card.id}>
+                            <div className="card-sub-container" >
+                                <div className="card-front">
+                                    <div className="payment-icon">
+                                        <IoIosCard />
+                                    </div>
                                     <p>{card.card_company}({card.card_type})</p>
-                                    <p>{card.name}</p>
-                                    <button className="form-button list-item" onClick={e => { e.preventDefault() }}>
+                                    <p className="card-name">{card.name}</p>
+                                </div>
+                                <div className="card-back" key={card.id}>
+                                    <button className="card-button list-item" onClick={e => { e.preventDefault() }}>
                                         <OpenModalMenuItem
 
                                             itemText={"Delete Card"}
                                             modalComponent={
                                                 <DeleteCardModal
                                                     id={id}
-                                                    cardId ={card.id}
+                                                    cardId={card.id}
                                                 />
                                             }
                                         />
                                     </button>
-                                    <button className="form-button" onClick={(e) => handleCurrent(e, id, card.id, card)}>Update Card</button>
+                                    <button className="card-button" onClick={(e) => handleCurrent(e, id, card.id, card)}>Update Card</button>
                                 </div>
                             </div>
-                        )
-                    })}
-                </div>
-                <NavLink to={`/users/${id}/cards/add`}><button className="mouse pay-button">Add new Card</button></NavLink>
+                        </div>
+                    )
+                })}
             </div>
+            <NavLink to={`/users/${id}/cards/add`}><button className="mouse card-button">Add new Card</button></NavLink>
         </section>
     )
 }
