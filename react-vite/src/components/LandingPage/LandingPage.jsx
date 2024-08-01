6cdea86777c3dad41931c7b003adf75c7dc5f2c6
Cece Potakey <cecepotakey24@gmail.com>
2024-08-01 19:22:26 -0400

    afrolive image added to landing page

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -8,6 +8,7 @@ function LandingPage() {
 
         <div className="payment-form-background">
             <div className="form-container">
+                <img src="https://res.cloudinary.com/dv9oyy79u/image/upload/v1722553954/IMG_0109_mhdhi7.png" alt="" />
                 <h1 className="form-title">Welcome to Afrolive</h1>
                 <LoginFormPage />
             </div>
